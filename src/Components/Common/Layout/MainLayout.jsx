98cacc6c55ed9f9c77fabc8b1f4ca449421363c6
Sazib <mahiburislamsazib@gmail.com>
2024-11-02 21:44:19 +0600

    banner section done

--- src/Components/Common/Layout/MainLayout.jsx
+++ src/Components/Common/Layout/MainLayout.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
+import Banner from '../../Page/Banner/Banner';
 
 const MainLayout = () => {
     return (
         <div>
             <div>
                 <Navbar></Navbar>
+            </div>
+            <div>
+                <Banner></Banner>
             </div>
             <Outlet></Outlet>
         </div>
--- src/Components/Page/Banner/Banner.jsx
+++ src/Components/Page/Banner/Banner.jsx
@@ -2,9 +2,19 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div>
-            
+        <div className="hero bg-base-200 max-w-[1170px] mx-auto sm:h-[70vh] rounded-3xl">
+        <div className="hero-content flex-col lg:flex-row-reverse gap-28">
+            <img src="https://i.ibb.co.com/jDLPxGt/pngwing-1.jpg" className="max-w-sm" alt="Hero" />
+            <div className="text-center sm:text-left">
+                <h1 className="text-4xl sm:text-5xl font-bold playfont mb-3 sm:mb-12 sm:leading-relaxed">
+                    Books to freshen up <br /> your bookshelf
+                </h1>
+                <a href="/listedbooks">
+                    <button className="btn bg-[#23BE0A] text-white mb-6">View The List</button>
+                </a>
+            </div>
         </div>
+    </div>
     );
 };
 
