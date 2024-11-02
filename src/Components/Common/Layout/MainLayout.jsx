feee0e0d32fe12a081c26861b4a47661328cc839
Sazib <mahiburislamsazib@gmail.com>
2024-11-02 21:18:42 +0600

    navbar compleat

---
+++ src/Components/Common/Layout/MainLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import { Outlet } from 'react-router-dom';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <div>
+                <Navbar></Navbar>
+            </div>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default MainLayout;
---
+++ src/Components/Common/Navbar/Navbar.jsx
@@ -0,0 +1,81 @@
+import { NavLink } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <div className="mt-4 mb-14 max-w-[1170px] mx-auto">
+      <div className="navbar bg-base-100">
+        {/* Navbar Start */}
+        <div className="navbar-start">
+          <div className="dropdown">
+            <button tabIndex="0" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                ></path>
+              </svg>
+            </button>
+            <ul
+              tabIndex="0"
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? 'text-[#23BE0A] font-bold' : 'font-bold'
+                }
+              >
+                Home
+              </NavLink>
+              <NavLink to="/listedbooks" className="font-bold">
+                Listed Books
+              </NavLink>
+              <NavLink to="/pagestoread" className="font-bold">
+                Pages to Read
+              </NavLink>
+            </ul>
+          </div>
+          <a className="text-xl sm:text-4xl font-black">Book Vibe</a>
+        </div>
+
+        {/* Navbar Center */}
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 flex gap-6 items-center">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? 'text-[#23BE0A] font-bold border border-[#23BE0A] rounded-lg p-3'
+                  : 'font-normal p-3'
+              }
+            >
+              Home
+            </NavLink>
+            <NavLink to="/listedbooks" className="font-normal p-3">
+              Listed Books
+            </NavLink>
+            <NavLink to="/pagestoread" className="font-normal p-3">
+              Pages to Read
+            </NavLink>
+          </ul>
+        </div>
+
+        {/* Navbar End */}
+        <div className="navbar-end flex gap-4">
+          <button className="btn bg-[#23BE0A] text-white">Sign In</button>
+          <button className="btn bg-[#59C6D2] text-white">Sign Up</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
